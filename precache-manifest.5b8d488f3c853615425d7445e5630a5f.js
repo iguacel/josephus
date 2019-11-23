4e251db9a578aa1c499f4f43b1b45d1cd472dc35
J.A. Iguacel <infoiguacel@gmail.com>
2019-11-23 18:40:42 +0100

    Updates

---
+++ precache-manifest.5b8d488f3c853615425d7445e5630a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ced1cae6752edb4c802b28f55813dd8",
+    "url": "/josephus/index.html"
+  },
+  {
+    "revision": "042adb7a3d6f448ce9e7",
+    "url": "/josephus/static/css/main.a63efd98.chunk.css"
+  },
+  {
+    "revision": "71d2cd09ba8fdd8be4ea",
+    "url": "/josephus/static/js/2.09adb09b.chunk.js"
+  },
+  {
+    "revision": "042adb7a3d6f448ce9e7",
+    "url": "/josephus/static/js/main.6121b42c.chunk.js"
+  },
+  {
+    "revision": "f4567367a11767e891a4",
+    "url": "/josephus/static/js/runtime-main.65f9152e.js"
+  }
+]);
